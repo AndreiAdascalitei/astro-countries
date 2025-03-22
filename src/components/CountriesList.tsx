25bab424f0e35957898d710eb3f2ae4f71345113
Andrei Adascalitei <andrei.adascalitei@stadlerpartners.com>
2025-03-22 13:48:47 +0200

    Add toggle control

--- src/components/CountriesList.tsx
+++ src/components/CountriesList.tsx
@@ -22,9 +22,24 @@ export default function CountriesList({ countries }: Props) {
 
   return (
     <div>
-      <button onClick={() => setShowLandlocked(!showLandlocked)}>
-        {showLandlocked ? "Show All" : "Show Landlocked"}
-      </button>
+       {/* 
+        Toggle Switch - took it from here: https://www.w3schools.com/howto/howto_css_switch.asp 
+        Best practice is to create a reusable UI component for this one, for later use. For simplicity, I'll keep it like this..
+      // */}
+      <div className={styles.toggleWrapper}>
+        <label className={styles.switch}>
+          <input
+            type="checkbox"
+            checked={showLandlocked}
+            onChange={() => setShowLandlocked(!showLandlocked)}
+          />
+          <span className={styles.slider}></span>
+        </label>
+        <span className={styles.label}>
+          Show Landlocked
+        </span>
+      </div>
+
       <div className={styles.grid}>
         {filteredCountries.map((country) => (
             <div key={country.name.common} className={styles.card}>
